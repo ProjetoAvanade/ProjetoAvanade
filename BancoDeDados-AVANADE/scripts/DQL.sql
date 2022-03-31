fae6c0a8815b308d90486a1b32830bc993b33144
Luiz Felipe Vera Cruz <39517928840@info.scs>
2022-03-31 11:15:32 -0300

    atualizacao dos scripts

--- BancoDeDados-AVANADE/scripts/DQL.sql
+++ BancoDeDados-AVANADE/scripts/DQL.sql
@@ -1,18 +1,13 @@
-USE AVANADE;
+USE [db-avanade];
 GO
 
 Select * From bicicletarios;
 GO
-
 Select * From reservas;
 GO
-
 Select * From usuarios;
 GO
-
 Select * From tipoUsuario;
 GO
-
 SELECT * FROM vagas
 GO;
-
