5913e9f4e1b0d3d29ea180f9d08f879dbe944524
Leonardo Souza De Castro <47073656808@info.scs>
2022-04-04 11:14:35 -0300

    Tela de Dashbord iniciada

--- avanade_projeto/src/pages/PainelADM.jsx
+++ avanade_projeto/src/pages/PainelADM.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { Doughnut } from 'react-chartjs-2';
 
 import img_perfil from '../assets/img/profile.svg'
 import icone_verde from '../assets/img/icone.svg'
@@ -26,11 +27,31 @@ export default function PainelADM() {
                             <img src={icone_verde} alt="Icone referente ao lucro do sistema" />
                         </div>
                     </div>
-                    <div></div>
-                    <div></div>
+
+                    <div className="box_info box_info_vermelho">
+                        <div className="infos">
+                            <span>Custos</span>
+                            <span>R$ 512,50</span>
+                            <span>+ 5,4% que na semana passada</span>
+                        </div>
+                        <div className="logo_box">
+                            <img src={icone_verde} alt="Icone referente ao lucro do sistema" />
+                        </div>
+                    </div>
+
+                    <div className="box_info box_info_amarelo">
+                        <div className="infos">
+                            <span>Usuarios Totais</span>
+                            <span>8.2K</span>
+                            <span>Taxa de crescimento de 8% ao mês</span>
+                        </div>
+                        <div className="logo_box">
+                            <img src={icone_verde} alt="Icone referente ao lucro do sistema" />
+                        </div>
+                    </div>
                 </div>
                 <div className="container_grafico">
-
+                    
                 </div>
             </section>
         </div>
