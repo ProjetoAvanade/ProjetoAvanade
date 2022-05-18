f2792c8d87e123f8bc20cac788a189d68e0e79f8
Leonardo Souza De Castro <47073656808@info.scs>
2022-05-18 11:08:29 -0300

    Funcionalidade de buscar usuario iniciada

--- src/pages/PainelADM.jsx
+++ src/pages/PainelADM.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 import { UserData } from '../teste'
@@ -13,37 +14,58 @@ import img_perfil from '../assets/img/img_perfil.png'
 
 import '../assets/css/PainelADM.css'
 
+
+
 export default function PainelADM() {
-    const [userData] = useState({
-        labels: UserData.map((data) => data.month),
-        datasets: [
-            {
-                label: "Lucros",
-                data: UserData.map((data) => data.userGain),
-                backgroundColor: [
-                    "#90F926",
-                ],
-            },
-            {
-                label: "Perdas",
-                data: UserData.map((data) => data.userLost),
-                backgroundColor: [
-                    "#F92626",
-                ],
-            },
-        ],
-    });
+    const [NomeUsuario, setNomeUsuario ] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [Email , setEmail] = useState("")
+    const [DataNascimento, setDataNascimento] = useState(Date.now);
+
+    function BuscarUsuario() {
+        axios('https://api-avanade.azurewebsites.net/api/Usuario', {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('usuario-login')
+            }
+        }).then((resposta) => {
+            if (resposta.status === 200) {
+                console.log(resposta.data)
+                setNomeUsuario(resposta.data.NomeUsuario)
+                setCpf(resposta.data.Cpf)
+                setEmail(resposta.data.Email)
+                setDataNascimento(resposta.data.DataNascimento)
+            }
+        }).catch(erro => console.log(erro))
+    }
+
+
+    function atualizar_conteudo() {
+        var x = document.getElementById("teste");
+        var y = document.getElementById("teste_perfil");
+        x.style.display = "block";
+        y.style.display = "none"
+    }
+    
+    function atualizar_conteudo_perfil() {
+        var x = document.getElementById("teste");
+        var y = document.getElementById("teste_perfil");
+        x.style.display = "none";
+        y.style.display = "block"
+    }
+
+    useEffect(BuscarUsuario(), []);
+
     return (
         <div className='Container_tela'>
             <section className='menu_sidebar'>
                 <img src={logo_login} alt="Logo do Bikecione" className='logo' />
 
                 <div className='box_secoes'>
-                    <div className='box_campo'>
+                    <div className='box_campo' onClick={() => atualizar_conteudo()}>
                         <img src={icon_dashboard} alt="Imagem para a tela de dashboard" />
                         <span>Dashboard</span>
                     </div>
-                    <div className='box_perfil'>
+                    <div className='box_perfil' onClick={() => atualizar_conteudo_perfil()}>
                         <img src={icon_perfil} alt="Imagem para a tela de perfil" />
                         <span>Perfil</span>
                     </div>
@@ -56,7 +78,7 @@ export default function PainelADM() {
                     </Link>
                 </div>
             </section>
-            <section className='conteudo'>
+            <section className='conteudo' id='teste'>
 
                 <div className='cabecalho'>
                     <h1 className='titulo'>Dashboard</h1>
@@ -68,7 +90,7 @@ export default function PainelADM() {
                 </section>
             </section>
 
-            <section className='conteudo_perfil'>
+            <section className='conteudo_perfil' id='teste_perfil'>
                 <div className='cabecalho'>
                     <h1 className='titulo'>Perfil</h1>
                 </div>
@@ -76,15 +98,15 @@ export default function PainelADM() {
                 <section className='container_perfil'>
                     <img src={img_perfil} alt="Foto de Perfil do usuario" className='img_user'/>
                     <div className='box_info_usuarios'>
-                        <span>Yuri Mitsugui Chiba</span>
-                        <span>000.000.000-00</span>
+                        <span>{NomeUsuario}</span>
+                        <span>{cpf}</span>
                         <div className='box_icon'>
                             <img src={icon_email} alt="Icone de Email" />
-                            <span>yurichiba@gmail.com</span>
+                            <span>{Email}</span>
                         </div>
                         <div className='box_icon_bolo'>
                             <img src={icon_bolo} alt="Icone de Aniversario" />
-                            <span>24/12/2004</span>
+                            <span>{DataNascimento}</span>
                         </div>
                     </div>
                 </section>
